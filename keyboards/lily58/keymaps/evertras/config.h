aa505bd5ad4c6dfef96879bd71f1bb656ed83be2
Brandon Fulljames <bfullj@gmail.com>
2021-08-04 23:01:59 +0900

    Enable auto shift

--- keyboards/lily58/keymaps/evertras/config.h
+++ keyboards/lily58/keymaps/evertras/config.h
@@ -44,3 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_SLEEP
 */
+
+// Auto shift timeout in milliseconds
+#define AUTO_SHIFT_TIMEOUT 150
+
