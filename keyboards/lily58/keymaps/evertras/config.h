927701bc57104a153a66c1f03091d2424c1f8d8f
Brandon Fulljames <brandon.fulljames@woven-planet.global>
2022-01-18 12:43:25 +0900

    Add commented option for OLED TIMEOUT

--- keyboards/lily58/keymaps/evertras/config.h
+++ keyboards/lily58/keymaps/evertras/config.h
@@ -48,3 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Auto shift timeout in milliseconds
 #define AUTO_SHIFT_TIMEOUT 100
 
+// 10 minutes before fading out
+//#define OLED_TIMEOUT 600000
+
